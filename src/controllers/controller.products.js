8a03e2ea9ff27644e2dbaa60200fb2ea9aa136d1
DaiC <daira_02@hotmail.com>
2024-02-01 15:48:49 -0300

    ultimos arreglos

--- src/controllers/controller.products.js
+++ src/controllers/controller.products.js
@@ -5,48 +5,78 @@ const router = Router();
 
 router.get('/', async (req, res) => {
     try {
-        const pageSize = 10;
-        const currentPage = parseInt(req.query.page) || 1;
-        const products = await productsModel
-            .find()
-            .skip((currentPage - 1) * pageSize)
-            .limit(pageSize);
-
-        const totalProducts = await productsModel.countDocuments();
-        const hasMorePages = currentPage * pageSize < totalProducts;
-
-        const hasPrevPag = currentPage > 1;
-        const hasNextPag = hasMorePages;
-        const prevLink = hasPrevPag ? `/productos?page=${currentPage - 1}` : null;
-        const nextLink = hasNextPag ? `/productos?page=${currentPage + 1}` : null;
+        const limit = parseInt(req.query.limit) || 10;
+        const page = parseInt(req.query.page) || 1;
+        const sortDirection = req.query.sort === 'desc' ? -1 : 1
+        const sort = req.query.sort ? { price: sortDirection } : undefined;
+        const filter = {};
 
 
-        const carritoId = obtenerOGenerarCarritoId(req);
+        if (req.query.category) {
+            filter.category = req.query.category;
+        }
 
-        res.render('productos', {
-            products: products,
-            hasPrevPag: hasPrevPag,
-            hasNextPag: hasNextPag,
-            prevLink: prevLink,
-            nextLink: nextLink,
-            carritoId: carritoId,
-        });
+
+        if (Boolean(req.query.available)) {
+            filter.available = req.query.available;
+        }
+
+
+        const {docs, pages, hasPrevPage, hasNextPage, prevPage, nextPage} = await productsModel.paginate(filter, {limit, page, sort, lean: true})
+        const products = docs
+        
+        res.render('home', { 
+            products,
+            totalPages: pages,
+            prevPage,
+            nextPage,
+            page,
+            hasPrevPage,
+            hasNextPage,
+            prevLink: hasPrevPage ? `?page=${prevPage}&limit=${limit}` : null,
+            nextLink: hasNextPage ? `?page=${nextPage}&limit=${limit}` : null,
+            style: 'index.css',
+        })
+        
     } catch (error) {
-        console.error('Error en la ruta /productos:', error);
+        console.error('Error en la ruta /home:', error);
         res.status(500).json({ status: 'error', error: 'Internal error' });
     }
 });
 
-function obtenerOGenerarCarritoId(req) {
-    if (req.session.carritoId) {
-        return req.session.carritoId;
+router.post('/add', async (req, res) => {
+    try {
+        const newProductData = req.body; 
+        const newProduct = await productsModel.create(newProductData);
+        res.status(201).json({ status: 'success', data: newProduct });
+    } catch (error) {
+        console.error('Error al agregar un producto:', error);
+        res.status(500).json({ status: 'error', error: 'Internal error' });
     }
+});
 
-    const nuevoCarritoId = Date.now().toString();
-    req.session.carritoId = nuevoCarritoId;
+router.put('/:productId/edit', async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        const updatedProductData = req.body;
+        const updatedProduct = await productsModel.findByIdAndUpdate(productId, updatedProductData, { new: true });
+        res.status(200).json({ status: 'success', data: updatedProduct });
+    } catch (error) {
+        console.error('Error al editar un producto:', error);
+        res.status(500).json({ status: 'error', error: 'Internal error' });
+    }
+});
 
-    return nuevoCarritoId;
-}
+router.delete('/:productId/delete', async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        await productsModel.findByIdAndDelete(productId);
+        res.status(204).end();
+    } catch (error) {
+        console.error('Error al eliminar un producto:', error);
+        res.status(500).json({ status: 'error', error: 'Internal error' });
+    }
+});
 
 export default router;
 
